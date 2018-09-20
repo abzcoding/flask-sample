0fd7f1b6f86074a9c996a30a1f1537aa7ccdf5cf
abzcoding <abzcoding@gmail.com>
2018-09-21 01:18:07 +0430

    updated test_client according to best practices

--- tests/test_client.py
+++ tests/test_client.py
@@ -9,10 +9,12 @@ class TestClient():
     if username is None:
       self.auth = None
     else:
-      self.auth = 'Basic ' + b64encode((username + ':' + password)
-                                       .encode('utf-8')).decode('utf-8')
+      self.auth = 'Basic ' + b64encode(
+          (username + ':' + password).encode('utf-8')).decode('utf-8')
 
-  def send(self, url, method='GET', data=None, headers={}):
+  def send(self, url, method='GET', data=None, headers=None):
+    if headers is None:
+      headers = {}
     headers = headers.copy()
     if self.auth is not None:
       headers['Authorization'] = self.auth
@@ -21,12 +23,12 @@ class TestClient():
     if data:
       data = json.dumps(data)
 
-    with self.app.test_request_context(url, method=method, data=data,
-                                       headers=headers):
+    with self.app.test_request_context(
+        url, method=method, data=data, headers=headers):
       try:
         rv = self.app.preprocess_request()
         if rv is None:
-            rv = self.app.dispatch_request()
+          rv = self.app.dispatch_request()
         rv = self.app.make_response(rv)
         rv = self.app.process_response(rv)
       except HTTPException as e:
@@ -40,17 +42,27 @@ class TestClient():
       json_data = None
     return rv, json_data
 
-  def get(self, url, headers={}):
+  def get(self, url, headers=None):
+    if headers is None:
+      headers = {}
     return self.send(url, 'GET', headers=headers)
 
-  def post(self, url, data, headers={}):
+  def post(self, url, data, headers=None):
+    if headers is None:
+      headers = {}
     return self.send(url, 'POST', data, headers=headers)
 
-  def put(self, url, data, headers={}):
+  def put(self, url, data, headers=None):
+    if headers is None:
+      headers = {}
     return self.send(url, 'PUT', data, headers=headers)
 
-  def delete(self, url, headers={}):
+  def delete(self, url, headers=None):
+    if headers is None:
+      headers = {}
     return self.send(url, 'DELETE', headers=headers)
 
-  def head(self, url, headers={}):
+  def head(self, url, headers=None):
+    if headers is None:
+      headers = {}
     return self.send(url, 'HEAD', headers=headers)
